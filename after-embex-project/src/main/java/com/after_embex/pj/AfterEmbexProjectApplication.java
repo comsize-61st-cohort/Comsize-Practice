da109bea5c603cfc1b1eb9382d849e75dbef2031
Nt-pass <tatsuya.nitatoge@gmail.com>
2026-06-30 17:17:39 +0900

    Spring Bootプロジェクトを新規追加
    after-embex-project

---
+++ after-embex-project/src/main/java/com/after_embex/pj/AfterEmbexProjectApplication.java
@@ -0,0 +1,13 @@
+package com.after_embex.pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterEmbexProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfterEmbexProjectApplication.class, args);
+	}
+
+}
